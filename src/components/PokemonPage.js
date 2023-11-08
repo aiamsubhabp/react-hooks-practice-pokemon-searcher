e7f596e0d50fb57d83aa996244ced79fabd4c89e
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-11-08 01:20:51 -0500

    add front sprite card to page

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:3001/pokemon')
+      .then(r => (r.json()))
+      .then(pokemonArray => setPokemon(pokemonArray))
+  }, [])
+
+  // const pokemonsToDisplay = pokemon.filter((poke) => {
+  //   return poke.name.toLowerCase().includes(sear)
+  // }) 
+
+
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
@@ -13,7 +27,7 @@ function PokemonPage() {
       <br />
       <Search />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemon = {pokemon} />
     </Container>
   );
 }
